da1a9f79ea873848f6f83dfb784fc932f4d03fcf
Tony Allard <tony.g.allard@gmail.com>
2017-11-01 21:49:51 +1030

    Added TILFactory

---
+++ src/popf2/pddl/TILFactory.h
@@ -0,0 +1,46 @@
+/*
+ * TILFactory.h
+ *
+ *  Created on: 20 October 2017
+ *      Author: tony
+ */
+
+#ifndef __TILFACTORY
+#define __TILFACTORY
+
+#include <string>
+#include <list>
+
+#include "TIL.h"
+
+#include "../RPGBuilder.h"
+
+namespace PDDL {
+
+class TILFactory {
+public:
+
+	static TILFactory * getInstance();
+
+	PDDL::TIL getTIL(Planner::RPGBuilder::FakeTILAction aTIL, double aTimestamp,
+		std::list<std::pair<std::string, std::string> > constants = std::list<
+				std::pair<std::string, std::string> >());
+private:
+	//Singleton Instance
+	static TILFactory * INSTANCE;
+	
+	//Private constructor
+	TILFactory(){};
+	//Singleton
+	TILFactory(TILFactory const & other) {
+	}
+	;
+	//Singleton
+	TILFactory& operator=(TILFactory const&) {
+	}
+	;
+};
+
+}
+
+#endif /* __TILFACTORY */
